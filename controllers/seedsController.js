9d296b6893443258e549c426f07d2df3ea446dc4
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-08-23 11:00:37 -0500

    added seed update and seed delete functions

--- controllers/seedsController.js
+++ controllers/seedsController.js
@@ -8,20 +8,49 @@ const Seed = require('../models').Seed;
 //show a list of all seeds
 router.get("/", (req, res) => {
     Seed.findAll({ order: ['id'] }).then((seeds) => {
-      res.render("showseeds.ejs", {
-        seeds: seeds,
-      });
+        res.render("showseeds.ejs", {
+            seeds: seeds,
+        });
     });
-  });
+});
 
 //create new seed 
 router.get("/new", (req, res) => {
     res.render('newseed.ejs');
 })
 //create new seed
-  router.post("/new", (req, res) => {
+router.post("/", (req, res) => {
     Seed.create(req.body).then((newSeed) => {
         res.redirect('/seeds');
     });
 });
-module.exports=router;
+
+// find seed for edit
+router.get('/:id', (req, res) => {
+    Seed.findByPk(req.params.id).then((seed) => {
+        console.log('in get id',seed,req.params.id);
+        res.render('editseed.ejs', {
+            seed: seed,
+            id: req.params.id
+        });
+    });
+});
+
+//edit seed
+router.put('/:id', (req, res) => {
+    console.log('in put seed',req.params)
+    Seed.update(req.body, {
+        where: { id: req.params.id },
+        returning: true,
+    }).then((seed) => {
+        res.redirect('/seeds');
+    });
+})
+
+router.delete('/:id', (req,res) => {
+    Seed.destroy({ where: { id: req.params.id } }).then(() => {
+        res.redirect("/seeds");
+      });
+    });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override');  //used for form to be able t
 //Middleware
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));  //this gives you the body of the request in a 
+app.use(methodOverride("_method"));
 
 //start at /gardens and tell it to use the gardens controller
 app.use('/gardens', require('./controllers/gardensController.js'));
