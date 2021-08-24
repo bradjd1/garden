be78dc56cbb745cdb835a2d5294fc04b4184c418
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-08-24 14:52:55 -0500

    set up a landing page in server.js at the root directory which can re-direct you to gardens or seeds

--- server.js
+++ server.js
@@ -8,6 +8,24 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));  //this gives you the body of the request in a 
 app.use(methodOverride("_method"));
 
+app.use((req,res,next) => {      //runs on all routes so put it at top
+    // console.log(`run for all routes: ${req.method} ${req.url}`);  //simple way to do basic logging
+     let logStr= `${req.method} ${req.url}`;
+     if(req.body) {
+         logStr += ` input data is: ${JSON.stringify(req.body)}`;
+     }
+     console.log(logStr);
+     next();                     //will go on to other routes
+ })
+ app.use(methodOverride('_method'));
+ 
+ app.get('/', (req, res) => {
+     res.render('gardenIndex.ejs', {
+ //        users: User
+     });
+ });
+
+
 //start at /gardens and tell it to use the gardens controller
 app.use('/gardens', require('./controllers/gardensController.js'));
 app.use('/seeds', require("./controllers/seedsController.js"));
